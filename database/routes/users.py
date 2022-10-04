8836996cdd5585cfe463cc967f90eb40eea754c7
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2022-10-03 22:15:15 -0700

    fetch user details endpoint created

--- database/routes/users.py
+++ database/routes/users.py
@@ -4,6 +4,20 @@ from database.models.models import User
 
 users = Blueprint('users', __name__)
 
+@users.route("/user", methods=['POST'])
+def fetch_user_details():
+    request_json = request.get_json()
+    email = request_json["email"]
+
+    get_user = db.session.query(User).filter(User.email == email).all()[0]
+    details = {
+        "username": get_user.username,
+        "email": get_user.email,
+        "uid": get_user.uid,
+        "created": get_user.created,
+    }
+    return jsonify(details)
+
 @users.route("/user/create", methods=['POST'])
 def user_create():
     """ create a user in the Users table """
@@ -17,3 +31,4 @@ def user_create():
     db.session.commit()
 
     return jsonify(success="User added!")
+
